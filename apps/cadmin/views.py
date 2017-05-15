2c422862c5e5ba7c46c95ede4c815cdf5f7979c0
彭伟 <1300896173@qq.com>
2017-05-15 14:21:13 +0800

    文章修改

--- apps/cadmin/views.py
+++ apps/cadmin/views.py
@@ -3,14 +3,7 @@
 ModelView文件
 """
 from flask_backstage import BaseModelView
-from flask_backstage import admin_create_required
 
 class PostView(BaseModelView):
-	column_labels = dict(title="标题", content="内容", time="发布时间", image='图片')
+	column_labels = dict(title="标题", content="内容", time="发布时间")
 	name = "文章"
-	image_list = ['image']
-	create_templates = 'frontend/create_post.html'
-	edit_templates = 'frontend/edit_post.html'
-
-	def is_accessible(self):
-		return admin_create_required(role=0)
--- apps/models/post.py
+++ apps/models/post.py
@@ -6,4 +6,3 @@ class Post(db.Document):
 	title = StringField(max_length=60)
 	content = StringField()
 	time = DateTimeField()
-	image = StringField(max_length=100)
